5bddc4fb70e9c0e4c2ded3402094db2d9bfa841e
SJ-Chuang <r08521606@ntu.edu.tw>
2021-08-23 23:19:43 +0800

    remove shell scripts

--- migrate.sh
+++
@@ -1,5 +0,0 @@
-sudo chown sj851109:root ../aiis db.sqlite3
-sudo rm -rf aicore/migrations/
-python3 manage.py makemigrations aicore
-python3 manage.py migrate aicore zero
-python3 manage.py migrate aicore
--- restart.sh
+++
@@ -1,2 +0,0 @@
-sudo chown apache:apache ../aiis db.sqlite3
-sudo systemctl restart httpd
